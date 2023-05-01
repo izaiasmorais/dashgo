19e495b75e0342bb919ecfe54f1a092adbeb9132
Izaias <izaiaslima356@gmail.com>
2023-05-01 12:47:42 -0300

    fix: apex chart options property

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -24,33 +24,33 @@ export function Chart() {
 		tooltip: {
 			enabled: false,
 		},
-		xaxis: {
-			type: "datetime",
-			axisBorder: {
-				color: theme.colors.gray[600],
-			},
-			axisTicks: {
-				color: theme.colors.gray[600],
-			},
-			categories: [
-				"2021-03-18T00:00:00.000Z",
-				"2021-03-19T00:00:00.000Z",
-				"2021-03-20T00:00:00.000Z",
-				"2021-03-21T00:00:00.000Z",
-				"2021-03-23T00:00:00.000Z",
-				"2021-03-24T00:00:00.000Z",
-				"2021-03-25T00:00:00.000Z",
-			],
-		},
-		fill: {
-			opacity: 0.3,
-			type: "gradient",
-			gradient: {
-				shade: "dark",
-				opacityGrom: "0.7",
-				opacityTo: "0.3",
-			},
-		},
+		// xaxis: {
+		// 	type: "datetime",
+		// 	axisBorder: {
+		// 		color: theme.colors.gray[600],
+		// 	},
+		// 	axisTicks: {
+		// 		color: theme.colors.gray[600],
+		// 	},
+		// 	categories: [
+		// 		"2021-03-18T00:00:00.000Z",
+		// 		"2021-03-19T00:00:00.000Z",
+		// 		"2021-03-20T00:00:00.000Z",
+		// 		"2021-03-21T00:00:00.000Z",
+		// 		"2021-03-23T00:00:00.000Z",
+		// 		"2021-03-24T00:00:00.000Z",
+		// 		"2021-03-25T00:00:00.000Z",
+		// 	],
+		// },
+		// fill: {
+		// 	opacity: 0.3,
+		// 	type: "gradient",
+		// 	gradient: {
+		// 		shade: "dark",
+		// 		opacityGrom: "0.7",
+		// 		opacityTo: "0.3",
+		// 	},
+		// },
 	};
 
 	const series = [{ name: "series1", data: [31, 120, 10, 28, 51, 18, 109] }];
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,4 @@
-import { Flex, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar";
 import { Header } from "../components/Header";
 import { Chart } from "../components/Chart";
@@ -11,41 +11,25 @@ export default function Dashboard() {
 			<Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
 				<Sidebar />
 
-				<Flex direction="column" w="100%">
-					<SimpleGrid
-						gap="4"
-						mb="6"
-						minChildWidth={320}
-						alignContent="flex-start"
-						gridColumn="2fr 1fr"
-					>
-						<Flex
-							bg="gray.800"
-							direction="column"
-							p="4"
-							fontWeight={500}
-							gap={2}
-							borderRadius={8}
-						>
-							<Text>Recebimentos do mês</Text>
-							{/* <Flex w="100%"> */}
-								<Chart />
-							{/* </Flex> */}
-						</Flex>
-
-						<Flex
-							bg="gray.800"
-							direction="column"
-							p="4"
-							fontWeight={500}
-							gap={2}
-							borderRadius={8}
-						>
-							<Text>Fluxo de investimento</Text>
-							<Chart />
-						</Flex>
-					</SimpleGrid>
-				</Flex>
+				<SimpleGrid
+					flex="1"
+					gap="4"
+					minChildWidth="320px"
+					alignItems="flex-start"
+				>
+					<Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+						<Text fontSize="lg" mb="4">
+							Vendas do mês
+						</Text>
+						<Chart />
+					</Box>
+					<Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
+						<Text fontSize="lg" mb="4">
+							Taxa de abertura
+						</Text>
+						<Chart />
+					</Box>
+				</SimpleGrid>
 			</Flex>
 		</Flex>
 	);
